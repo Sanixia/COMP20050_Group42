9cb88322c2c9e6ccd86ecb117ce16bc2e3e40db6
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-02-03 10:03:59 +0000

    Cascadia main controller class update

--- src/Cascadia.java
+++ src/Cascadia.java
@@ -1,11 +1,80 @@
+import java.util.Scanner;
+
+public class Cascadia
+{
+    public static void controller()
+    {
+        /* welcome message */
+
+        System.out.println("Welcome to Cascadia!");
+
+        /* ask how many players */
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nEnter the number of players (2-4): ");
+        int player_count = in.nextInt();
+
+        /* enter player names */
+
+        System.out.println("\nPlayer 1 please enter your name: ");
+        String player_one = in.next();
+
+        System.out.println("\nPlayer 2 please enter your name: ");
+        String player_two = in.next();
+
+        if (player_count < 3) {
+
+        }
+
+        /* rng player order */
+        /* show 4 habitat tiles and 4 paired tokens */
+
+        /* starter habitats randomly generated for each player */
 
 
+        // upon a players turn :
+        /* pick tile
+           next command to show their habitat
+           quit command to terminate program */
 
 
-public class Cascadia{
+        // NOTES:
+        /* there are only 5 possible starter tiles to rng (check manual)
+           60 double habitat tiles (10 unique types) ,
+           25 keystone tiles (5 unique types) */
 
-    public static void main(String[] args) {
-
+        /* 2 players: 43 tiles }
+           3 players: 63 tiles } 20 per player +3 for habitat
+           4 players: 83 tiles } */
+    }
+    public static void main(String[] args)
+    {
+        controller();
     }
 
+    /* welcome message */
+
+    /* ask how many players */
+    /* enter player names */
+
+    /* rng player order */
+    /* show 4 habitat tiles and 4 paired tokens */
+
+    /* starter habitats randomly generated for each player */
+
+
+    // upon a players turn :
+        /* pick tile
+           next command to show their habitat
+           quit command to terminate program */
+
+
+    // NOTES:
+        /* there are only 5 possible starter tiles to rng (check manual)
+           60 double habitat tiles (10 unique types) ,
+           25 keystone tiles (5 unique types) */
+
+        /* 2 players: 43 tiles }
+           3 players: 63 tiles } 20 per player +3 for habitat
+           4 players: 83 tiles } */
 }
