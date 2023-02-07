19c581c2f950029c62bbc152c43e7c220a8551aa
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-02-07 13:59:47 +0000

    wildlife tokens now implemented

--- src/Habitat_Tiles.java
+++ src/Habitat_Tiles.java
@@ -27,17 +27,19 @@ public class Habitat_Tiles
      */
 
     static ArrayList<String> biome = new ArrayList<>(
-            Arrays.asList("F","F","F","F","F", "W","W","W","W","W", "R","R","R","R","R", "M","M","M","M","M", "P","P","P","P","P",
-                    "FW","FW","FW","FW","FW","FW", "FR","FR","FR","FR","FR","FR", "FM","FM","FM","FM","FM","FM", "FP","FP","FP","FP","FP","FP",
-                    "WR","WR","WR","WR","WR","WR", "WM","WM","WM","WM","WM","WM", "WP","WP","WP","WP","WP","WP",
-                    "RM","RM","RM","RM","RM","RM", "RP","RP","RP","RP","RP","RP", "MP","MP","MP","MP","MP","MP")
+            Arrays.asList("F","F","F","F","F",  "W","W","W","W","W",  "R","R","R","R","R",  "M","M","M","M","M",  "P","P","P","P","P",
+                    "FW","FW","FW","FW","FW","FW",  "FR","FR","FR","FR","FR","FR",  "FM","FM","FM","FM","FM","FM",
+                    "FP","FP","FP","FP","FP","FP",  "WR","WR","WR","WR","WR","WR",  "WM","WM","WM","WM","WM","WM",
+                    "WP","WP","WP","WP","WP","WP",  "RM","RM","RM","RM","RM","RM",  "RP","RP","RP","RP","RP","RP",
+                    "MP","MP","MP","MP","MP","MP")
     );
 
     static ArrayList<String> animals = new ArrayList<>(
-            Arrays.asList("E","B","B","F","F", "H","F","F","S","S", "S","B","B","E","E", "B","E","E","H","H", "F","E","E","S","S",
-                    "BES","BF","BS","FH","ES","EH", "EFH","FS","BE","EH","BF","BS", "BEF","BEH","EF","EH","BF","BH", "EFS","BF","FS","BE","EF","ES",
-                    "BHS","BS","HS","FH","BH","FS", "BHS","BES","BS","HS","EH","EF", "EFS","FHS","FH","EF","ES","HS",
-                    "BHS","BE","BH","EH","HS","BS", "BFS","BFH","FH","EH","ES","BF", "BEF","BFS","BS","EH","FH","ES")
+            Arrays.asList("E","B","B","F","F",  "H","F","F","S","S",  "S","B","B","E","E",  "B","E","E","H","H",  "F","E","E","S","S",
+                    "BES","BF","BS","FH","ES","EH",  "EFH","FS","BE","EH","BF","BS",  "BEF","BEH","EF","EH","BF","BH",
+                    "EFS","BF","FS","BE","EF","ES",  "BHS","BS","HS","FH","BH","FS",  "BHS","BES","BS","HS","EH","EF",
+                    "EFS","FHS","FH","EF","ES","HS",  "BHS","BE","BH","EH","HS","BS",  "BFS","BFH","FH","EH","ES","BF",
+                    "BEF","BFS","BS","EH","FH","ES")
     );
 
 
--- src/Wildlife_Tokens.java
+++ src/Wildlife_Tokens.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Wildlife_Tokens {
+    static ArrayList<String> tokens = new ArrayList<>(
+            Arrays.asList("B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B",
+                    "E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E",
+                    "F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F",
+                    "H","H","H","H","H","H","H","H","H","H","H","H","H","H","H","H","H","H","H","H",
+                    "S","S","S","S","S","S","S","S","S","S","S","S","S","S","S","S","S","S","S","S")
+    );
+
+    public static void main(String[] args) {
+        for (int i=0; i<100; i++) {
+            System.out.println((i+1) +"  "+ tokens.get(i));
+        }
+    }
 }
