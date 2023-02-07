ee7ac27307be4b9717995419c36f1fbb074fcb3d
Sanixia <roshan.varghese@ucdconnect.ie>
2023-02-07 11:34:47 +0000

    added switch cases to starter tiles

--- src/Tile_Printer.java
+++ src/Tile_Printer.java
@@ -1,2 +1,20 @@
 public class Tile_Printer {
+
+    public static void starter_tile_printout(String starterType){
+
+        switch (starterType){
+            case "Forest":
+
+            case "Mountain":
+
+            case "Prairie":
+
+            case "Wetland":
+
+            case "River":
+
+        }
+    }
+
+    //public static void
 }
