b5f4540281636542d15f85207a071e24e851254d
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-02-09 06:49:53 +0000

    Colours added to letters tile printer mainly finished, will require updates for stage 2 Comments added
    I finally rest and watch the sun rise on a grateful universe

--- src/Experimental_Printer.java
+++ src/Experimental_Printer.java
@@ -1,16 +1,21 @@
 public class Experimental_Printer
 {
-    private static final String sp12_R = "            " + Tile_Colours.RESET;
-    private static final String sp6_R = "      " + Tile_Colours.RESET;
-    private static final String sp3_R = "   " + Tile_Colours.RESET;
+    // Declaring constants to make printing more efficient
+    // Naming scheme: sp6_R refers to 6 empty spaces with a reset
+    private static final String reset = Tile_Colours.RESET;
+    private static final String sp12_R = "            " + reset;
+    private static final String sp6_R = "      " + reset;
+    private static final String sp3_R = "   " + reset;
     private static final String sp6 = "      ";
     private static final String sp3 = "   ";
 
 
+    // function which gets the necessary biome colours before passing on to main print function
+    // Note: cases aren't commutative i.e. FW works but WF does not
     public static String print_tile_setup(String biome_tile, int rotation, String animals)
     {
         String col1 = null, col2 = null;
-        String animals_with_space = animals + "   ";
+        String animals_with_space = animals + "   ";        // adding 3 empty spaces to avoid program crashing in middle_section function
 
         switch (biome_tile) {
             case "F": col1=Tile_Colours.FOREST_COLOUR;  break;
@@ -36,13 +41,13 @@ public class Experimental_Printer
 
     public static String tile_printout(String colour1, String colour2, int rotation, String animals)
     {
-        // Single biome tiles
+        // Printing keystone tiles
         if (colour2 == null)
         {
             return long_section(colour1) + body(colour1, colour1, animals) + long_section(colour1);
         }
 
-        // double biome tiles
+        // Switch case for printing double biome tiles in each of their rotations
         else {
             switch (rotation) {
                 case 1: return long_section(colour1) + body(colour2, colour1, animals) + long_section(colour2);
@@ -64,15 +69,17 @@ public class Experimental_Printer
         return null;
     }
 
-
+    // Prints out one coloured roof / floors
     public static String long_section(String colour) {
         return colour + sp12_R + "\n";
     }
 
+    // Prints out two coloured roof / floor for when the double biomes are divided vertically
     public static String long_section_dual(String colour1, String colour2) {
         return colour1 + sp6 + colour2 +sp6_R + "\n";
     }
 
+    // Prints out the body of a tile; the sides, animals and empty spaces
     public static String body(String colour1, String colour2, String animals) {
         return sides_section(colour1) + middle_section(animals.charAt(0)) + middle_section(animals.charAt(1)) + sides_section(colour2) + "\n"
                 + sides_section(colour1) + middle_section(animals.charAt(2)) + sp3 + sides_section(colour2) +"\n";
@@ -82,44 +89,37 @@ public class Experimental_Printer
         return colour + sp3_R;
     }
 
+    // Printing out animal (or empty) character spaces with their respective colours
     public static String middle_section(char animal) {
+        String colour;
         switch (animal) {
-            case 'B':
-                return " B ";
-            case 'E':
-                return " E ";
-            case 'F':
-                return " F ";
-            case 'H':
-                return " H ";
-            case 'S':
-                return " S ";
+            case 'B': colour = Tile_Colours.BEAR_COLOUR;
+                return " " + colour + "B" + reset + " ";
+            case 'E': colour = Tile_Colours.ELK_COLOUR;
+                return " " + colour + "E" + reset + " ";
+            case 'F': colour = Tile_Colours.FOX_COLOUR;
+                return " " + colour + "F" + reset + " ";
+            case 'H': colour = Tile_Colours.HAWK_COLOUR;
+                return " " + colour + "H" +  reset + " ";
+            case 'S': colour = Tile_Colours.SALMON_COLOUR;
+                return " " + colour + "S" +  reset + " ";
             default:
                 return sp3;
         }
     }
 
 
-    public static void main(String[] args)
+    public static void main(String[] args)  // some tests
     {
         System.out.println(print_tile_setup("FW", 1,"EHS") + "POSITION 1");
         System.out.println(print_tile_setup("FW", 2,"B") + "POSITION 2");
         System.out.println(print_tile_setup("FW", 3,"BS") + "POSITION 3");
         System.out.println(print_tile_setup("FW", 4,"EFH") + "POSITION 4");
         System.out.println(print_tile_setup("FW", 5,"FH") + "POSITION 5");
-        System.out.println(print_tile_setup("FW", 6,"S") + "POSITION 6");
+        System.out.println(print_tile_setup("FW", 6,"S") + "POSITION 6\n\n");
         System.out.println(print_tile_setup("RM", 3,"H"));
-        System.out.println(print_tile_setup("RP", 5,"BFS"));
+        System.out.println(print_tile_setup("R", 3,"BE"));
+        System.out.println(print_tile_setup("WP", 5,"BFS"));
         System.out.println(print_tile_setup("M", 3,"BE"));
-/*
-        String m = "E";
-        m = m + "  ";
-        char a = m.charAt(1);
-        char b;
-        char c;
-
-        System.out.println(a);
- */
     }
-
 }
