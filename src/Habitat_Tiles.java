b0097947dcfcdb74f176305a7c5e4caca69e1fc8
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-02-10 09:41:50 +0000

    removed chunk of unnecessary comments

--- src/Habitat_Tiles.java
+++ src/Habitat_Tiles.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Habitat_Tiles {
+public class Habitat_Tiles
+{
     /* System of using 2 arrays to create the habitat tiles, one for biomes, the other for the animals that can be placed
        A given index for the biome array will correspond to the animals array
 
@@ -11,23 +12,28 @@ public class Habitat_Tiles {
        All tiles are exact to the ones from the original game */
 
     static ArrayList<String> biome = new ArrayList<>(
-            Arrays.asList("F", "F", "F", "F", "F", "W", "W", "W", "W", "W", "R", "R", "R", "R", "R", "M", "M", "M", "M", "M", "P", "P", "P", "P", "P",
-                    "FW", "FW", "FW", "FW", "FW", "FW", "FR", "FR", "FR", "FR", "FR", "FR", "FM", "FM", "FM", "FM", "FM", "FM",
-                    "FP", "FP", "FP", "FP", "FP", "FP", "WR", "WR", "WR", "WR", "WR", "WR", "WM", "WM", "WM", "WM", "WM", "WM",
-                    "WP", "WP", "WP", "WP", "WP", "WP", "RM", "RM", "RM", "RM", "RM", "RM", "RP", "RP", "RP", "RP", "RP", "RP",
-                    "MP", "MP", "MP", "MP", "MP", "MP")
+            Arrays.asList("F","F","F","F","F",  "W","W","W","W","W",  "R","R","R","R","R",  "M","M","M","M","M",  "P","P","P","P","P",
+                    "FW","FW","FW","FW","FW","FW",  "FR","FR","FR","FR","FR","FR",  "FM","FM","FM","FM","FM","FM",
+                    "FP","FP","FP","FP","FP","FP",  "WR","WR","WR","WR","WR","WR",  "WM","WM","WM","WM","WM","WM",
+                    "WP","WP","WP","WP","WP","WP",  "RM","RM","RM","RM","RM","RM",  "RP","RP","RP","RP","RP","RP",
+                    "MP","MP","MP","MP","MP","MP")
     );
 
     static ArrayList<String> animals = new ArrayList<>(
-            Arrays.asList("E", "B", "B", "F", "F", "H", "F", "F", "S", "S", "S", "B", "B", "E", "E", "B", "E", "E", "H", "H", "F", "E", "E", "S", "S",
-                    "BES", "BF", "BS", "FH", "ES", "EH", "EFH", "FS", "BE", "EH", "BF", "BS", "BEF", "BEH", "EF", "EH", "BF", "BH",
-                    "EFS", "BF", "FS", "BE", "EF", "ES", "BHS", "BS", "HS", "FH", "BH", "FS", "BHS", "BES", "BS", "HS", "EH", "EF",
-                    "EFS", "FHS", "FH", "EF", "ES", "HS", "BHS", "BE", "BH", "EH", "HS", "BS", "BFS", "BFH", "FH", "EH", "ES", "BF",
-                    "BEF", "BFS", "BS", "EH", "FH", "ES")
+            Arrays.asList("E","B","B","F","F",  "H","F","F","S","S",  "S","B","B","E","E",  "B","E","E","H","H",  "F","E","E","S","S",
+                    "BES","BF","BS","FH","ES","EH",  "EFH","FS","BE","EH","BF","BS",  "BEF","BEH","EF","EH","BF","BH",
+                    "EFS","BF","FS","BE","EF","ES",  "BHS","BS","HS","FH","BH","FS",  "BHS","BES","BS","HS","EH","EF",
+                    "EFS","FHS","FH","EF","ES","HS",  "BHS","BE","BH","EH","HS","BS",  "BFS","BFH","FH","EH","ES","BF",
+                    "BEF","BFS","BS","EH","FH","ES")
     );
-}
 
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 85; i++) {
+            System.out.println((i+1) + " biome: " + biome.get(i) + "   animal: " + animals.get(i));
+        }
+    };
+};
 
 
 
--- src/Starter_Tile_Printer.java
+++ src/Starter_Tile_Printer.java
@@ -115,80 +115,9 @@ public class Starter_Tile_Printer {
                         + "\n" + Tile_Colours.PRAIRIE_COLOUR + smallBlank + Tile_Colours.RESET + Tile_Colours.BEAR_COLOUR + " B    " + Tile_Colours.FOREST_COLOUR + smallBlank + Tile_Colours.RESET + Tile_Colours.WETLAND_COLOUR + smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + "      " + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET
                         + "\n" + Tile_Colours.FOREST_COLOUR + bigBlank + Tile_Colours.RESET + Tile_Colours.WETLAND_COLOUR + bigBlank + Tile_Colours.RESET
                 );
-
-
-
-                /*     USED FOR SPRINT 2
-
-                System.out.println( blankSix + Tile_Colours.MOUNTAIN_COLOUR + bigBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.MOUNTAIN_COLOUR+ smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " B  " + Tile_Colours.ELK_COLOUR + "H " + Tile_Colours.BLUE_BACKGROUND + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " S    " + Tile_Colours.BLUE_BACKGROUND + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND + bigBlank+ Tile_Colours.RESET
-                );
-
-                System.out.println( blankSix + Tile_Colours.MOUNTAIN_COLOUR + bigBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND+ smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " B  " + Tile_Colours.ELK_COLOUR + "H " + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND + smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " S    " + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND + "            " + Tile_Colours.RESET
-                );
-
-                System.out.println( blankSix + Tile_Colours.BLUE_BACKGROUND + "      " + Tile_Colours.MOUNTAIN_COLOUR + "      " + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND+ smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " B  " + Tile_Colours.ELK_COLOUR + "H " + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND + smallBlank + Tile_Colours.RESET + Tile_Colours.ELK_COLOUR + " S    " + Tile_Colours.MOUNTAIN_COLOUR + smallBlank + Tile_Colours.RESET
-                        + blankSix + Tile_Colours.BLUE_BACKGROUND + "      " + Tile_Colours.MOUNTAIN_COLOUR + "      " + Tile_Colours.RESET
-                );
-
-                 */
-
-        }
-
-    }
-
-
-    // Idea: array.split ex, FM_B for forest mountian bear, then
-    /*public static String[] habitat_tile_setup(String biomeTile) {
-
-        return switch (biomeTile) {
-            case "FW" -> new String[]{Tile_Colours.FOREST_COLOUR, Tile_Colours.WETLAND_COLOUR};
-            case "FR" -> new String[]{Tile_Colours.FOREST_COLOUR, Tile_Colours.RIVER_COLOUR};
-            case "FM" -> new String[]{Tile_Colours.FOREST_COLOUR, Tile_Colours.MOUNTAIN_COLOUR};
-            case "FP" -> new String[]{Tile_Colours.FOREST_COLOUR, Tile_Colours.PRAIRIE_COLOUR};
-            case "WR" -> new String[]{Tile_Colours.WETLAND_COLOUR, Tile_Colours.RIVER_COLOUR};
-            case "WM" -> new String[]{Tile_Colours.WETLAND_COLOUR, Tile_Colours.MOUNTAIN_COLOUR};
-            case "WP" -> new String[]{Tile_Colours.WETLAND_COLOUR, Tile_Colours.PRAIRIE_COLOUR};
-            case "RM" -> new String[]{Tile_Colours.RIVER_COLOUR, Tile_Colours.MOUNTAIN_COLOUR};
-            case "RP" -> new String[]{Tile_Colours.RIVER_COLOUR, Tile_Colours.PRAIRIE_COLOUR};
-            case "MP" -> new String[]{Tile_Colours.MOUNTAIN_COLOUR, Tile_Colours.PRAIRIE_COLOUR};
-            case "F" -> new String[]{Tile_Colours.FOREST_COLOUR};
-            case "W" -> new String[]{Tile_Colours.WETLAND_COLOUR};
-            case "R" -> new String[]{Tile_Colours.RIVER_COLOUR};
-            case "M" -> new String[]{Tile_Colours.MOUNTAIN_COLOUR};
-            case "P" -> new String[]{Tile_Colours.PRAIRIE_COLOUR};
-            default -> new String[]{};
-        };
-
-
-
-    }
-
-    public static void habitat_tile_printout(String[] biomeTile, String animalTile) {
-        if (biomeTile.length == 1){
-            System.out.println(blankSix + biomeTile[0] + bigBlank + Tile_Colours.RESET
-                    + blankSix + biomeTile[0] + smallBlank + Tile_Colours.RESET + Tile_Colours.FOX_COLOUR + " " + "F" + "  " + Tile_Colours.RESET + "  " + biomeTile[0] + smallBlank + Tile_Colours.RESET
-                    + blankSix + biomeTile[0] + smallBlank + Tile_Colours.RESET  + "      " + biomeTile[0] + smallBlank + Tile_Colours.RESET
-                    + blankSix + biomeTile[0] + bigBlank + Tile_Colours.RESET
-            );
-        }
-        else{
-            System.out.println(biomeTile[0] + bigBlank + Tile_Colours.RESET
-                    + "\n" + biomeTile[0] + smallBlank + Tile_Colours.RESET + Tile_Colours.SALMON_COLOUR + " " + "S" + "  " + Tile_Colours.ELK_COLOUR + "E" + " " + biomeTile[1] + smallBlank + Tile_Colours.RESET
-                    + "\n" + biomeTile[0] + smallBlank + Tile_Colours.RESET + Tile_Colours.HAWK_COLOUR + " " + "H" + "    " + biomeTile[1] + smallBlank + Tile_Colours.RESET
-                    + "\n" + biomeTile[1] + bigBlank + Tile_Colours.RESET
-            );
         }
     }
 
-     */
 
     public static String token_printer(String animal) {
         String colour;
