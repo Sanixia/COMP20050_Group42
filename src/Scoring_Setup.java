e55f0b207a8dca7b450ca3d016dc84d6a78f8794
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-04-07 17:30:41 +0100

    changed to suit junit test

--- src/Scoring_Setup.java
+++ src/Scoring_Setup.java
@@ -57,15 +57,20 @@ public class Scoring_Setup extends Board
         b[0][0] = new Board("RM", "e", 0);
         b[0][2] = new Board("RM", "e", 0);
         b[0][3] = new Board("RM", "e", 0);
+        b[0][4] = new Board("RM", "d", 0);
         b[0][5] = new Board("RM", "e", 0);
         b[0][6] = new Board("RM", "e", 0);
         b[0][7] = new Board("RM", "e", 0);
+        b[2][1] = new Board("RM", "h", 0);
         b[2][2] = new Board("RM", "e", 0);
         b[2][3] = new Board("RM", "e", 0);
+        b[2][5] = new Board("RM", "b", 0);
+        b[3][3] = new Board("RM", "f", 0);
         b[3][4] = new Board("RM", "e", 0);
         b[3][2] = new Board("RM", "e", 0);
         b[4][0] = new Board("RM", "e", 0);
         b[4][1] = new Board("RM", "e", 0);
+        b[4][3] = new Board("RM", "s", 0);
         b[4][4] = new Board("RM", "e", 0);
         b[4][5] = new Board("RM", "e", 0);
         b[5][0] = new Board("RM", "e", 0);
@@ -94,13 +99,13 @@ public class Scoring_Setup extends Board
 
 
         // TODO                b e f h s
-        int[] scoring_cards = {3,3,1,3,1};
+        int[] scoring_cards = {0,3,0,0,0};
         Board.print_boards(b, 8, 6, 1);
-        scoring_setups(b, 1,  scoring_cards);
+        int score = scoring_setups(b, 1,  scoring_cards);
     }
 
 
-    public static void scoring_setups(Board[][]board, int odd, int[] scoring_cards) {       // temporary for testing purposes
+    public static int scoring_setups(Board[][]board, int odd, int[] scoring_cards) {       // temporary for testing purposes
         setBoard(board);
         setOdd(odd);
         int fox_num=0, hawk_num=0, bear_num=0, salmon_score=0, elk_num=0;
@@ -110,18 +115,18 @@ public class Scoring_Setup extends Board
             for (int j=0; j<MAXSIZE; j++) {
                 Board t = board[i][j];
                 if( t!=null && !t.getAnimals().isBlank() && t.getAnimals().charAt(0)=='f') {            // FOX
-                    //System.out.println("fox    ["+i+"]["+j+"] " + fox_scoring_cards(i, j, scoring_cards[2]));
+                    System.out.println("fox    ["+i+"]["+j+"] " + fox_scoring_cards(i, j, scoring_cards[2]));
                     fox_num += fox_scoring_cards(i, j, 1);
                 }
                 else if(t!=null && !t.getAnimals().isBlank() && t.getAnimals().charAt(0)=='h') {        // HAWK
-                    //System.out.println("hawk   ["+i+"]["+j+"] " + hawk_scoring_cards(i, j, scoring_cards[3]));
+                    System.out.println("hawk   ["+i+"]["+j+"] " + hawk_scoring_cards(i, j, scoring_cards[3]));
                     if(scoring_cards[3]==3) {
                         hawk_num += hawk_score_calculate(hawk_scoring_cards(i, j, scoring_cards[3]), scoring_cards[3]);
                     }
                     else{
                         hawk_num += hawk_scoring_cards(i, j, 2);
                     }
-                    //hawk_num += hawk_scoring_cards(i, j, 3);
+                    hawk_num += hawk_scoring_cards(i, j, 3);
                 }
                 else if(t!=null && !t.getAnimals().isBlank() && t.getAnimals().charAt(0)=='b') {        // BEAR
                     System.out.println("bear   ["+i+"]["+j+"] " + bear_scoring_cards(i, j, scoring_cards[0]));
@@ -132,7 +137,7 @@ public class Scoring_Setup extends Board
                     else bear_num += bear_scoring_cards(i, j, scoring_cards[0]);
                 }
                 else if(t!=null && !t.getAnimals().isBlank() && t.getAnimals().charAt(0)=='s') {        // SALMON
-                    //System.out.println("salmon ["+i+"]["+j+"] " + salmon_scoring_cards(i, j, 3));
+                    System.out.println("salmon ["+i+"]["+j+"] " + salmon_scoring_cards(i, j, 3));
                     salmon_score += salmon_score_calculate(salmon_scoring_cards(i, j, 2), 2);
                 }
                 if(t!=null && t.getAnimals().charAt(0)=='e') {                                          // ELK
@@ -147,10 +152,10 @@ public class Scoring_Setup extends Board
             }
         }
         int fox_score = fox_score_calculate(fox_num, scoring_cards[2]);
-        int hawk_score = hawk_score_calculate(hawk_num, 2);
+        int hawk_score = hawk_score_calculate(hawk_num, scoring_cards[3]);
         int bear_score = bear_score_calculate(bear_num, bear_shapes, scoring_cards[0]);
         int elk_score = elk_score_calculate(elk_num, elk_shapes, scoring_cards[1]); // insert bear scoring
-        int salmon_score2 = salmon_score_calculate(salmon_score, 3);
+        int salmon_score2 = salmon_score_calculate(salmon_score, scoring_cards[4]);
         System.out.println("\nTotal Fox score:" + fox_score);
         System.out.println("Total Hawk score:" + hawk_num);
         System.out.println("Total Bear score:" + bear_score);
@@ -158,9 +163,7 @@ public class Scoring_Setup extends Board
         System.out.println("Total Elk score:" + elk_score);
         System.out.println("Total Elk num:" + elk_num);
 
-        for (int i=0; i<e; i++) {
-            System.out.println("Elk board [" + i + "] "+elk_shapes[i]);
-        }
+        return fox_score + hawk_score + bear_score + elk_score + salmon_score2;
     }
 
 
@@ -170,7 +173,8 @@ public class Scoring_Setup extends Board
         return switch (card) {
             case 1 -> Scoring_Fox.fox_scoring_1(x, y);
             case 2 -> Scoring_Fox.fox_scoring_2(x, y);
-            default -> Scoring_Fox.fox_scoring_3(x, y);
+            case 3 -> Scoring_Fox.fox_scoring_3(x, y);
+            default -> 0;
         };
     }
 
@@ -178,7 +182,8 @@ public class Scoring_Setup extends Board
         return switch (card) {
             case 1 -> Scoring_Hawk.hawk_scoring_1(x, y);
             case 2 -> Scoring_Hawk.hawk_scoring_2(x, y);
-            default -> Scoring_Hawk.hawk_scoring_3(x, y);
+            case 3 -> Scoring_Hawk.hawk_scoring_3(x, y);
+            default -> 0;
         };
     }
 
@@ -186,7 +191,8 @@ public class Scoring_Setup extends Board
         return switch (card) {
             case 1 -> Scoring_Bear.bear_scoring_1(x, y);
             case 2 -> Scoring_Bear.bear_scoring_2(x, y);
-            default -> Scoring_Bear.bear_scoring_3(x, y);
+            case 3 -> Scoring_Bear.bear_scoring_3(x, y);
+            default -> 0;
         };
     }
 
@@ -195,7 +201,8 @@ public class Scoring_Setup extends Board
         return switch (card) {
             case 1 -> elk_score_calculate(Scoring_Elk.elk_scoring_1(x, y, pos), null, card);
             case 2 -> elk_score_calculate(Scoring_Elk.elk_scoring_2(x, y), null, card);
-            default -> Scoring_Elk.elk_scoring_3(x, y);
+            case 3 -> Scoring_Elk.elk_scoring_3(x, y);
+            default -> 0;
         };
     }
 
@@ -206,13 +213,17 @@ public class Scoring_Setup extends Board
 
 
     public static int fox_score_calculate(int f, int card) {
-        if (card == 2) {
-            if (f == 0) return 0;
-            if (f == 1) return 3;
-            if (f == 2) return 5;
-            return 7;
+        switch (card) {
+            case 0:
+                return 0;
+            case 2:
+                if (f == 0) return 0;
+                if (f == 1) return 3;
+                if (f == 2) return 5;
+                return 7;
+            default:
+                return f;
         }
-        return f;
     }
 
     public static int hawk_score_calculate(int h, int card) {
@@ -237,26 +248,28 @@ public class Scoring_Setup extends Board
                 if (h == 6) return 20;
                 if (h == 7) return 24;
                 return 28;
-            default:
+            case 3:
                 return h*3;
+            default:
+                return 0;
         }
 
     }
 
     public static int bear_score_calculate(int b, int[] bear_shapes, int card) {
         switch (card) {
-            case (1):
+            case 1:
                 b = b / 2;
                 if (b == 0) return 0;
                 if (b == 1) return 4;
                 if (b == 2) return 11;
                 if (b == 3) return 19;
                 return 27;
-            case(2):
+            case 2:
                 b = b / 3;
                 if (b == 0) return 0;
                 return b*10;
-            default:
+            case 3:
                 int one=0, two=0, three=0, three_b=0, bonus=0;
                 for (int bear_shape : bear_shapes) {
                     if (bear_shape == 1) one++;
@@ -269,6 +282,8 @@ public class Scoring_Setup extends Board
                 three += three_b;
                 if (one>0 && two>0 && three>0) bonus=3;
                 return one*2 + two*5 + three*8 + bonus;
+            default:
+                return 0;
         }
     }
 
@@ -290,7 +305,7 @@ public class Scoring_Setup extends Board
                 if (e == 6) return 18;
                 if (e == 7) return 23;
                 return 28;
-            default:
+            case 3:
                 int one=0, two=0, three=0, four=0;
                 for (int elk_shape : elk_shapes) {
                     if (elk_shape == 1) one++;
@@ -302,7 +317,7 @@ public class Scoring_Setup extends Board
                 three = three/3;
                 four = four/2;
                 return one*2 + two*5 + three*9 + four*13;
-
+            default: return 0;
         }
     }
 
@@ -323,13 +338,15 @@ public class Scoring_Setup extends Board
                 if (s == 2) return 4;
                 if (s == 3) return 8;
                 return 12;
-            default:
+            case 3:
                 if (s == 0) return 0;
                 if (s == 1) return 2;
                 if (s == 2) return 4;
                 if (s == 3) return 9;
                 if (s == 4) return 11;
                 return 17;
+            default:
+                return 0;
         }
     }
 
--- src/Scoring_SetupTest.java
+++ src/Scoring_SetupTest.java
@@ -1,33 +1,24 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Scoring_SetupTest {
-   /* public static void main(String[] args) {
-        Board[][] b = new Board[26][26];
-        b[0][1] = new Board("RM", "s", 0);
-        b[0][2] = new Board("RM", "b", 0);
-        b[0][3] = new Board("RM", "s", 0);
-        b[0][4] = new Board("RM", "b", 0);
-        b[1][1] = new Board("RM", "s", 0);
-        b[1][2] = new Board("R", "s", 0);
-        b[1][3] = new Board("R", "e", 0);
-        b[2][1] = new Board("R", "b", 0);
-        b[2][2] = new Board("R", "b", 0);
-        b[2][3] = new Board("R", "s", 0);
-        b[2][5] = new Board("R", "b", 0);
-        b[3][1] = new Board("R", "b", 0);
-        b[3][2] = new Board("R", "f", 0);
-        b[3][3] = new Board("R", "b", 0);
-        b[3][4] = new Board("R", "b", 0);
-        b[3][5] = new Board("R", "s", 0);
-        b[4][3] = new Board("R", "b", 0);
+        @Test
+        void FoxTest() {
+                Board[][] b = new Board[20][20];
 
-        // TODO                b e f h s
-        int[] scoring_cards = {3,1,1,1,1};
-        Board.print_boards(b, 8, 5, 1);
-        scoring_setups(b, 1,  scoring_cards);
+                b[0][2] = new Board("F", "f", 0);
+                b[0][3] = new Board("F", "f", 0);
+                b[2][2] = new Board("F", "f", 0);
+                b[2][3] = new Board("F", "f", 0);
+                b[3][4] = new Board("F", "f", 0);
+                b[3][2] = new Board("F", "f", 0);
 
 
-    }
+                int[] scoring_cards = {0,0,1,0,0};
+                Board.print_boards(b, 8, 6, 1);
+                int fox_score = Scoring_Setup.scoring_setups(b, 1,  scoring_cards);
+                assertEquals(6, fox_score);
 
-    */
+        }
 }
