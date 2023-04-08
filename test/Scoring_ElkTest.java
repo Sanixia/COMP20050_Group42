c32a28a27a3f159e815acf9ed5bcd97601e5e4ab
Sanixia <roshan.varghese@ucdconnect.ie>
2023-04-08 12:30:01 +0100

    elk A and B testing done

--- test/Scoring_ElkTest.java
+++ test/Scoring_ElkTest.java
@@ -72,6 +72,58 @@ public class Scoring_ElkTest {
         Board[][] b = new  Board[26][26];
         int[] scoring_cards = {0,2,0,0,0};
 
+        // 8 elk
+        // 28 score
+        b[0][0] = new Board("W", "e", 0);
+        b[0][1] = new Board("W", "e", 0);
+        b[1][1] = new Board("W", "e", 0);
+        b[1][2] = new Board("W", "e", 0);
+        b[1][3] = new Board("W", "e", 0);
+        b[0][4] = new Board("W", "e", 0);
+        b[1][5] = new Board("W", "e", 0);
+        b[2][3] = new Board("W", "e", 0);
+        b[3][2] = new Board("W", "e", 0);
+
+
+        // 5 elk scored
+        // 14 score
+        b[0][6] = new Board("W", "e", 0);
+        b[1][5] = new Board("W", "e", 0);
+        b[2][5] = new Board("W", "e", 0);
+        b[1][6] = new Board("W", "f", 0);
+        b[2][6] = new Board("W", "e", 0);
+        b[2][7] = new Board("W", "e", 0);
+
+
+        // 2 elk scored in straight diagonal line whilst surrounded by habitat tiles
+        // 4 score
+        b[4][5] = new Board("W", "e", 0);
+        b[4][6] = new Board("W", "E", 0);
+        b[5][5] = new Board("W", "e", 0);
+        b[5][6] = new Board("W", "f", 0);
+        b[5][4] = new Board("W", "F", 0);
+        b[4][4] = new Board("W", "s", 0);
+        b[3][4] = new Board("W", "b", 0);
+        b[3][5] = new Board("W", "H", 0);
+
+        // 1 elk
+        // 2 score
+        b[4][7] = new Board("W", "e", 0);
+
+
+
+        Board.print_boards(b, 8, 6, 1);
+        int elk_score = Scoring_Setup.scoring_setups(b, 1,  scoring_cards);
+
+        // 28 + 14 + 4 + 2 = 48
+        assertEquals(48, elk_score);
+    }
+
+    @Test
+    void elk_scoring_C() {
+        Board[][] b = new  Board[26][26];
+        int[] scoring_cards = {0,1,0,0,0};
+
         // 1 elk scored
         // 2 score
         b[0][2] = new Board("W", "e", 0);
@@ -125,35 +177,6 @@ public class Scoring_ElkTest {
         int elk_score = Scoring_Setup.scoring_setups(b, 1,  scoring_cards);
 
         // 2 + 5 + 9 + 13 + 9 + 5 + 5 = 48
-        assertEquals(48, elk_score);
-    }
-
-    @Test
-    void elk_scoring_C() {
-        Board[][] b = new  Board[26][26];
-        int[] scoring_cards = {0,1,0,1,0};
-
-        b[0][2] = new Board("F", "f", 0);
-        b[0][3] = new Board("F", "f", 0);
-        b[2][2] = new Board("F", "f", 0);
-        b[2][3] = new Board("F", "f", 0);
-        b[3][4] = new Board("F", "f", 0);
-        b[3][2] = new Board("F", "f", 0);
-
-        Board.print_boards(b, 8, 6, 1);
-        int elk_score = Scoring_Setup.scoring_setups(b, 1,  scoring_cards);
-        assertEquals(5, elk_score);
-
-        b[0][1] = new Board("F", "f", 0);
-        b[0][3] = new Board("F", "f", 0);
-        b[0][4] = new Board("F", "f", 0);
-        b[1][3] = new Board("F", "f", 0);
-        b[2][1] = new Board("F", "f", 0);
-        b[3][0] = new Board("F", "f", 0);
-        b[3][1] = new Board("F", "f", 0);
-
-        Board.print_boards(b, 8, 6, 1);
-        elk_score = Scoring_Setup.scoring_setups(b, 1,  scoring_cards);
-        assertEquals(12, elk_score);
+        assertEquals(0, elk_score);
     }
 }
