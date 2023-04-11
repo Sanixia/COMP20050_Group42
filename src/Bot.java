34c1020a992e71a1fea85775f492ca9bf8124d31
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2023-04-11 10:03:05 +0100

    em0ty bot class added

---
+++ src/Bot.java
@@ -0,0 +1,14 @@
+public class Bot {
+    public static void bot_place_tile() {
+
+    }
+
+    public static void bot_place_animal_token() {
+
+    }
+
+    public static void bot_menu_choice() {
+
+    }
+    // etc.
+}
